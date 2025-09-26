3f736551fdefb6ca23d4b33f4920c050424fd13e
Heinriches <heydrich.institut@gmail.com>
2025-09-25 23:40:31 -0300

    Create Example_10.py

---
+++ (udemy) Python Developer/74 - Iterations and Selections/Example_10.py
@@ -0,0 +1,7 @@
+# Complete the code to generate all possible
+# pair-combinations of numbers within the range
+# of 1 to 4, inclusive:
+
+for i in range (1, 5):
+    for j in range (1, 5):
+        print(i, j)
