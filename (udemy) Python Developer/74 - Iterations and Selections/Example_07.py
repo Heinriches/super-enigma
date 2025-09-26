ff4ca11c00477b742a227c03e1a71bda5ec960dd
Heinriches <heydrich.institut@gmail.com>
2025-09-25 23:34:47 -0300

    Create Example_07.py

---
+++ (udemy) Python Developer/74 - Iterations and Selections/Example_07.py
@@ -0,0 +1,19 @@
+#Ball:
+#red
+#green
+#Pen:
+#red
+#green
+
+# Exercise 01:
+#				Complete the code
+#				to get the output
+#				shown above:
+
+objects = ['Ball', 'Pen']
+colors = ['red', 'green']
+
+for object in objects:
+    print(object + ':')
+    for color in colors:
+        print(color)
