e421f59f2e22aa0243aeb85fc22e723443c78785
Heinriches <heydrich.institut@gmail.com>
2025-09-25 23:19:30 -0300

    Create Example_05.py

---
+++ (udemy) Python Developer/74 - Iterations and Selections/Example_05.py
@@ -0,0 +1,9 @@
+# Code a nested loop to create a list of repetitive
+# tasks for each day:
+
+days = ['Monday', 'Tuesday', 'Wednesday']
+tasks = ['Gym', 'Homework', 'Pool']
+
+for day in days:
+    for task in tasks:
+        print(day, task)
