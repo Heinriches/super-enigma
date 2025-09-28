68591b6ce1f8f503ea13f698425a76be2dd654c0
Heinriches <heydrich.institut@gmail.com>
2025-09-27 23:20:20 -0300

    Create Example_02.py

---
+++ (udemy) Python Developer/077 - Module Quiz/Example_02.py
@@ -0,0 +1,10 @@
+# MISTAKE REPETITION
+
+# Shorthand-operators are specially useful in iterations.
+# Complete the code to raise each price in the list by five.
+
+prices = [15, 19, 24, 8, 5]
+
+for i in prices:
+    i += 5
+    print(i)
