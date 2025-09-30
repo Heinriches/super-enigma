6b5b8d4b7ebde19d9111d99310673da1f78c4e72
Heinriches <heydrich.institut@gmail.com>
2025-09-29 23:38:38 -0300

    Update Example_03.py

--- (udemy) Python Developer/079 - Function Arguments/Example_03.py
+++ (udemy) Python Developer/079 - Function Arguments/Example_03.py
@@ -1,4 +1,7 @@
 # A function can be an argument for another function.
 
 print(type("word"))
-# type("word") is an argument for this print() function.
+# type("word") is the argument for this print() function.
+
+print(range(5))
+# range(5) is the argument for this print() function.
