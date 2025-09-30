8db912edbb49538228c670b8997084fefbcf192c
Heinriches <heydrich.institut@gmail.com>
2025-09-29 23:36:08 -0300

    Create Example_02.py

---
+++ (udemy) Python Developer/079 - Function Arguments/Example_02.py
@@ -0,0 +1,8 @@
+# Functions can take operations as arguments.
+# As an example, the print() function can
+# accept math, logical, and comparison operations.
+
+print(55 * 3) # math
+print(5 > 7) # comparison
+print(True and False) # logical
+print("motor" +"bike") # concatenation
