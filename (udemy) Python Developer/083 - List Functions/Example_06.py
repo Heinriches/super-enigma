32759ecbe5f22ae198fc88e2565dad613e2f482a
Heinriches <heydrich.institut@gmail.com>
2025-10-02 23:37:39 -0300

    Create Example_06.py

---
+++ (udemy) Python Developer/083 - List Functions/Example_06.py
@@ -0,0 +1,9 @@
+# The pop() functions removes an element from a list.
+
+# That position indicated by the index is the only argument
+# that the pop() function accepts.
+
+items = ["book", "pen", "pencil"]
+items.pop(1)
+print(items)
+print(items[1])
