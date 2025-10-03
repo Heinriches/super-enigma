73ed0cc85288ae5968f4cc42a4d0fb517a9a73d1
Heinriches <heydrich.institut@gmail.com>
2025-10-03 18:24:50 -0300

    Create Example_01.py

---
+++ (udemy) Python Developer/086 - Custom Functions/Example_01.py
@@ -0,0 +1,16 @@
+# To use your own functions, you need to define them first.
+
+# Once a function has been defined, you can call it as many
+# times as you need.
+
+# The greet() function below contains code to display a nice
+# message when called.
+
+
+#function definition
+def greet():
+    print("Hello from a function.")
+    print("Have a great day!")
+    
+#function call
+greet()
