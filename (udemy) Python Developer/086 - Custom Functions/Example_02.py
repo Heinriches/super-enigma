e4e72528cb7a1ef287a50fb9d55cb5fd4dfa3ddd
Heinriches <heydrich.institut@gmail.com>
2025-10-03 18:31:36 -0300

    Create Example_02.py

---
+++ (udemy) Python Developer/086 - Custom Functions/Example_02.py
@@ -0,0 +1,13 @@
+# A function might require arguments to complete its tasks.
+# Arguments are put inside the parenthesis() following the
+# function name.
+
+
+#function definition
+def personal_greet(name):
+    print("Hello, " , name)
+    print("Have a great day!")
+
+#function calls
+personal_greet("Sarah")
+personal_greet("Henry")
