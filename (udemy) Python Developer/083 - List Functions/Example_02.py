18bf816a3f0d5daa659766bdc4a82c94f543b8fd
Heinriches <heydrich.institut@gmail.com>
2025-10-02 23:26:41 -0300

    Create Example_02.py

---
+++ (udemy) Python Developer/083 - List Functions/Example_02.py
@@ -0,0 +1,8 @@
+# The append(0 function adds a new item to the end of a list.
+
+# 'append()' is called using dot-notation, because it's
+# specific to lists.
+
+songs = ["Yesterday", "Hello", "Believer"]
+songs.append("Imagine")
+print(songs)
