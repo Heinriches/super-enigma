88342478629a682167f361c10812babf54e021e8
Heinriches <heydrich.institut@gmail.com>
2025-10-02 23:23:17 -0300

    Update Example_01.py

--- (udemy) Python Developer/083 - List Functions/Example_01.py
+++ (udemy) Python Developer/083 - List Functions/Example_01.py
@@ -1,2 +1,7 @@
+# len(0 is one of the most useful built-in functions.
+
+# len() stands for 'length' and, when used on lists,
+# it returns the number of items in the list.
+
 movies = ["Avatar", "Titanic", "Avengers"]
 print(len(movies))
