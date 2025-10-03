208d9855669391c650eeda0365eba0d042db593b
Heinriches <heydrich.institut@gmail.com>
2025-10-02 23:32:35 -0300

    Create Example_04.py

---
+++ (udemy) Python Developer/083 - List Functions/Example_04.py
@@ -0,0 +1,7 @@
+# The insert() function allows you to add
+# an element to a list, at a specific position.
+
+items = ["book", "pen", "pencil"]
+items.insert(2, "marker")
+print(items)
+print(items[2])
