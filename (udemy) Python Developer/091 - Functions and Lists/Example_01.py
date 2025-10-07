49d0786de7778c17ed7505931fe4fd1576e1fc3a
Heinriches <heydrich.institut@gmail.com>
2025-10-06 21:53:29 -0300

    Create Example_01.py

---
+++ (udemy) Python Developer/091 - Functions and Lists/Example_01.py
@@ -0,0 +1,10 @@
+# You can create a custom function that takes
+# a list as an argument. This code will display
+# the entire menu on a single output line.
+
+my_menu = ["Pizza", "Burger", "Salad"]
+
+def show_menu(menu):
+    print(menu)
+
+show_menu(my_menu)
