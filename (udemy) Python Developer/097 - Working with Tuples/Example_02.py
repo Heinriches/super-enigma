55b130fe349157c647bf9a9da71733cc04fe3bcc
Heinriches <heydrich.institut@gmail.com>
2025-10-11 23:54:24 -0300

    Create Example_02.py

---
+++ (udemy) Python Developer/097 - Working with Tuples/Example_02.py
@@ -0,0 +1,8 @@
+# You can use tuples in any control flow structures,
+# such as if-else statements and loops, just
+# as you would with lists.
+
+points =(12, 14, 9, 10, 9)
+for point in points:
+    if point > 10:
+        print(point, ": passed")
