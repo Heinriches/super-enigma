0513530e40f378ec87e73760d7804ea950013cdc
Heinriches <heydrich.institut@gmail.com>
2025-10-11 23:49:24 -0300

    Create Example_01.py

---
+++ (udemy) Python Developer/097 - Working with Tuples/Example_01.py
@@ -0,0 +1,11 @@
+# Tuples, like lists, can contain duplicate
+# elements.
+
+# You can use the 'count()' function to
+# calculate thenumber of occurences of
+# an item in a tuple.
+
+scores = (7, 9, 9, 8, 9)
+print("# of 7:", scores.count(7))
+print("# of 9:", scores.count(9))
+print("# of 2:", scores.count(2))
